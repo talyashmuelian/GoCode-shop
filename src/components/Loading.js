c4b7cdc10f612a73e21d64dd26d4f748eab570a2
talya535@gmail.com <talya535@gmail.com>
2021-08-09 23:48:34 +0300

    exe4: fetch+loading

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+const Lopding = () => {
+  return (
+    <main className="spinner-examples">
+      <div className="example">
+        <span className="smooth spinner" />
+      </div>
+    </main>
+  );
+};
+export default Lopding;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import "./components/Header.css";
 import "./components/Products.css";
 import "./components/Product.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./components/Loading.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
